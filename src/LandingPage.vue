b98a2bd8ae45b5167379feeff10a8079eb946286
ezegyfa2 <darvasvok@gmail.com>
2021-11-20 14:07:01 +0100

    Adding component

---
+++ src/LandingPage.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    include LandingPage.pug
+</template>
+
+<script>
+    export default {
+        /*props: {
+            content: {
+                type: String
+            }
+        }*/
+    }
+</script>
+
+<style type="text/css">
+    
+</style>
