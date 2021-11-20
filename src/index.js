5e0378f4f30be20dc348b70b08f686ea366bf503
ezegyfa2 <darvasvok@gmail.com>
2021-11-20 13:53:19 +0100

    Adding packages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 export default {
 	install(Vue) {
+		require('text-contents-vue-components').default.install(Vue);
+		require('grayscale-vue-components').default.install(Vue);
+		require('description-vue-components').default.install(Vue);
+		require('helper-vue-components').default.install(Vue);
 	}
 };
