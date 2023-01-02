817b235a8db730b16d87ac5837264f1fd71d7bba
Darvas Istvan <darvasvok@gmail.com>
2023-01-02 21:36:03 +0200

    1.0.4

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ export default {
 		require('description-vue-components').default.install(Vue);
 		require('helper-vue-components').default.install(Vue);
 
-		Vue.component('grayscale-contact', require('./Contact/Contact/Component.vue').default);
-		Vue.component('grayscale-contact-card', require('./Contact/Card/Component.vue').default);
-		Vue.component('grayscale-medium-text-content', require('./MediumTextContent/Component.vue').default);
-		Vue.component('grayscale-large-text-content', require('./LargeTextContent/Component.vue').default);
+		registerVueComponent('grayscale-contact', require('./Contact/Contact/Component.vue').default, Vue);
+		registerVueComponent('grayscale-contact-card', require('./Contact/Card/Component.vue').default, Vue);
+		registerVueComponent('grayscale-medium-text-content', require('./MediumTextContent/Component.vue').default, Vue);
+		registerVueComponent('grayscale-large-text-content', require('./LargeTextContent/Component.vue').default, Vue);
 
-		Vue.component('grayscale-about', require('./About/Component.vue').default);
-		Vue.component('grayscale-header', require('./Header/Component.vue').default);
-		Vue.component('grayscale-navigation-bar', require('./NavigationBar/CollapsableNavigationBar/Component.vue').default);
-		Vue.component('grayscale-navigation-link', require('./NavigationBar/NavigationLink/Component.vue').default);
-		Vue.component('grayscale-signup', require('./Signup/Component.vue').default);
+		registerVueComponent('grayscale-about', require('./About/Component.vue').default, Vue);
+		registerVueComponent('grayscale-header', require('./Header/Component.vue').default, Vue);
+		registerVueComponent('grayscale-navigation-bar', require('./NavigationBar/CollapsableNavigationBar/Component.vue').default, Vue);
+		registerVueComponent('grayscale-navigation-link', require('./NavigationBar/NavigationLink/Component.vue').default, Vue);
+		registerVueComponent('grayscale-signup', require('./Signup/Component.vue').default, Vue);
 
-		Vue.component('grayscale-landing-page', require('./LandingPage/Component.vue').default);
+		registerVueComponent('grayscale-landing-page', require('./LandingPage/Component.vue').default, Vue);
 	}
 };
