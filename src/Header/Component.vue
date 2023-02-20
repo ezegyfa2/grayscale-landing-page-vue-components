7eee1e03479f71a11bc70e7d1c7b6d5aca93becb
ezegyfa2 <darvasvok@gmail.com>
2023-02-20 21:39:41 +0200

    1.0.6

--- src/Header/Component.vue
+++ src/Header/Component.vue
@@ -1,5 +1,5 @@
 <script>
-    import Header from './../../../helper-vue-components/src/Header/Component.vue'
+    import Header from './../../../helper-vue-components/src/Headers/Header/Component.vue'
 
     export default {
         mixins: [
